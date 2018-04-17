fdf527d6e5ef236d1222b876ce77fa711d12fbe9
Adrian Kozłowski <adrian.kozlowski@outlook.com>
2018-04-18 00:27:36 +0200

    Persistence.xml contents and dependencies

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -1,7 +1,12 @@
 package pl.sda;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
-
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("JPA");
+        EntityManager entityManager = emf.createEntityManager();
     }
 }
